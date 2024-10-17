f59ddae29a3358c7960953c66c241d7af7b97793
jakesjordan00 <119546212+jakesjordan00@users.noreply.github.com>
2024-10-17 18:11:10 -0400

    adding playoff endpoint

--- Playoffs.aspx
+++ Playoffs.aspx
@@ -10,11 +10,11 @@
     <div class="col-lg-4">     
         <h4>Select seasons to load Playoff data</h4>
         <asp:CheckBoxList ID="chkSeasons" runat="server" RepeatColumns="2" CellPadding="5">
-            <asp:ListItem Enabled="true" Value="0022000001-0022001080-2020" Text="2020-2021"></asp:ListItem>
-            <asp:ListItem Enabled="true" Value="0022100001-0022101230-2021" Text="2021-2022"></asp:ListItem>
-            <asp:ListItem Enabled="true" Value="0022200001-0022201230-2022" Text="2022-2023"></asp:ListItem>
-            <asp:ListItem Enabled="true" Value="0022300001-0022301230-2023" Text="2023-2024"></asp:ListItem>
-            <asp:ListItem Enabled="true" Value="0022400001-0022401230-2024" Text="2024-2025"></asp:ListItem>
+            <asp:ListItem Enabled="true" Value="20" Text="2020-2021"></asp:ListItem>
+            <asp:ListItem Enabled="true" Value="21" Text="2021-2022"></asp:ListItem>
+            <asp:ListItem Enabled="true" Value="22" Text="2022-2023"></asp:ListItem>
+            <asp:ListItem Enabled="true" Value="23" Text="2023-2024"></asp:ListItem>
+            <asp:ListItem Enabled="true" Value="24" Text="2024-2025"></asp:ListItem>
         </asp:CheckBoxList>
         <asp:Button ID="btnLoad" runat="server" Text="Load Playoff Data" height="75px" Width="300px" Font-Size="XX-Large" OnClick="btnLoad_Click" 
         style="color:cornflowerblue; background-color:black; border-color:black; text-decoration:none;  text-align:center;border-radius: 15px; border: 3px solid grey;"/>
--- Playoffs.aspx.cs
+++ Playoffs.aspx.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static NBAdb.FirstTimeLoad;
 
 namespace NBAdb
 {
@@ -16,7 +20,105 @@ namespace NBAdb
 
         protected void btnLoad_Click(object sender, EventArgs e)
         {
+            List<string> seasons = new List<string>();
+            foreach (ListItem item in chkSeasons.Items)
+            {
+                if (item.Selected)
+                {
+                    seasons.Add(item.Value);
+                }
+            }
+            if (seasons.Count == 0)
+            {
+                lblSeasonResult.Text = "Please select a season";
+                lblSeasonResult.ForeColor = System.Drawing.Color.IndianRed;
+            }
+            else
+            {
+                for (int i = 0; i < seasons.Count; i++)
+                {
+                    PlayoffSeries(Int32.Parse(seasons[i]));
+                }
+            }
+        }
 
+        protected void PlayoffSeries(int season)
+        {
+            var client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
+            string seasonEndpoint = "https://stats.nba.com/stats/playoffbracket?LeagueID=00&SeasonYear=20" + season + "&State=2";
+            try
+            {
+                WebRequest RequestSeasonPlayoffSeries = WebRequest.Create(seasonEndpoint);
+                WebResponse ResponseSeasonPlayoffSeries = RequestSeasonPlayoffSeries.GetResponse();
+                string json = client.DownloadString(seasonEndpoint);
+                PlayoffBracketResponse playoffData = JsonConvert.DeserializeObject<PlayoffBracketResponse>(json);
+                string breakp = "";
+            }
+            catch
+            {
+
+            }
         }
     }
+    public class Meta
+    {
+        public int version { get; set; }
+        public string request { get; set; }
+        public string time { get; set; }
+    }
+
+    public class PlayoffBracketSeries
+    {
+        public string seriesId { get; set; }
+        public int roundNumber { get; set; }
+        public int seriesNumber { get; set; }
+        public string seriesConference { get; set; }
+        public string seriesText { get; set; }
+        public int seriesStatus { get; set; }
+        public int seriesWinner { get; set; }
+        public int highSeedId { get; set; }
+        public string highSeedCity { get; set; }
+        public string highSeedName { get; set; }
+        public string highSeedTricode { get; set; }
+        public int highSeedRank { get; set; }
+        public int highSeedSeriesWins { get; set; }
+        public int highSeedRegSeasonWins { get; set; }
+        public int highSeedRegSeasonLosses { get; set; }
+        public int lowSeedId { get; set; }
+        public string lowSeedCity { get; set; }
+        public string lowSeedName { get; set; }
+        public string lowSeedTricode { get; set; }
+        public int lowSeedRank { get; set; }
+        public int lowSeedSeriesWins { get; set; }
+        public int lowSeedRegSeasonWins { get; set; }
+        public int lowSeedRegSeasonLosses { get; set; }
+        public int displayOrderNumber { get; set; }
+        public int displayTopTeam { get; set; }
+        public int displayBottomTeam { get; set; }
+        public string poRoundDesc { get; set; }
+        public string nextGameId { get; set; }
+        public string nextGameNumber { get; set; }
+        public string nextGameDateTimeEt { get; set; }
+        public string nextGameDateTimeUTC { get; set; }
+        public int nextGameStatus { get; set; }
+        public string nextGameStatusText { get; set; }
+        public int nextGameBroadcasterId { get; set; }
+        public string nextGameBroadcasterDisplay { get; set; }
+        public string lastCompletedGameId { get; set; }
+    }
+
+    public class Bracket
+    {
+        public string leagueId { get; set; }
+        public string seasonYear { get; set; }
+        public string bracketType { get; set; }
+        public List<PlayoffBracketSeries> playoffBracketSeries { get; set; }
+    }
+
+    public class PlayoffBracketResponse
+    {
+        public Meta meta { get; set; }
+        public Bracket bracket { get; set; }
+    }
+
 }
