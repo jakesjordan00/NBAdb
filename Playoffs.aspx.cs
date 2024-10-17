be3787de33a53744ffca67d5bd05998e88322661
jakesjordan00 <119546212+jakesjordan00@users.noreply.github.com>
2024-10-17 18:44:28 -0400

    adding playing

--- Playoffs.aspx.cs
+++ Playoffs.aspx.cs
@@ -45,15 +45,22 @@ namespace NBAdb
         protected void PlayoffSeries(int season)
         {
             var client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
-            string seasonEndpoint = "https://stats.nba.com/stats/playoffbracket?LeagueID=00&SeasonYear=20" + season + "&State=2";
+            string playoffEndpoint = "https://stats.nba.com/stats/playoffbracket?LeagueID=00&SeasonYear=20" + season + "&State=2";
+            string playinEndpoint = "https://stats.nba.com/stats/playoffbracket?LeagueID=00&SeasonYear=20" + season + "&State=2";
             try
             {
-                WebRequest RequestSeasonPlayoffSeries = WebRequest.Create(seasonEndpoint);
+                
+                WebRequest RequestSeasonPlayoffSeries = WebRequest.Create(playoffEndpoint);
                 WebResponse ResponseSeasonPlayoffSeries = RequestSeasonPlayoffSeries.GetResponse();
-                string json = client.DownloadString(seasonEndpoint);
-                PlayoffBracketResponse playoffData = JsonConvert.DeserializeObject<PlayoffBracketResponse>(json);
+                string jsonPlayoff = client.DownloadString(playoffEndpoint);
+                PlayoffBracketResponse playoffData = JsonConvert.DeserializeObject<PlayoffBracketResponse>(jsonPlayoff);
 
-                for(int i = 0; i < playoffData.bracket.playoffBracketSeries.Count; i++)
+                WebRequest RequestSeasonPlayinSeries = WebRequest.Create(playinEndpoint);
+                WebResponse ResponseSeasonPlayinSeries = RequestSeasonPlayinSeries.GetResponse();
+                string jsonPlayin = client.DownloadString(playinEndpoint);
+                PlayoffBracketResponse playinData = JsonConvert.DeserializeObject<PlayoffBracketResponse>(jsonPlayin);
+
+                for (int i = 0; i < playoffData.bracket.playoffBracketSeries.Count; i++)
                 {
                     int games = playoffData.bracket.playoffBracketSeries[i].highSeedSeriesWins + playoffData.bracket.playoffBracketSeries[i].lowSeedSeriesWins;
                 }
@@ -113,12 +120,42 @@ namespace NBAdb
         public string lastCompletedGameId { get; set; }
     }
 
+    public class PlayInBracketSeries
+    {
+        public string matchupType { get; set; }
+        public string conference { get; set; }
+        public int highSeedId { get; set; }
+        public string highSeedCity { get; set; }
+        public string highSeedName { get; set; }
+        public string highSeedTricode { get; set; }
+        public int highSeedRegSeasonWins { get; set; }
+        public int highSeedRegSeasonLosses { get; set; }
+        public int highSeedRank { get; set; }
+        public int lowSeedId { get; set; }
+        public string lowSeedCity { get; set; }
+        public string lowSeedName { get; set; }
+        public string lowSeedTricode { get; set; }
+        public int lowSeedRegSeasonWins { get; set; }
+        public int lowSeedRegSeasonLosses { get; set; }
+        public int lowSeedRank { get; set; }
+        public string seriesId { get; set; }
+        public string nextGameId { get; set; }
+        public string nextGameDateTimeEt { get; set; }
+        public string nextGameDateTimeUTC { get; set; }
+        public int nextGameStatus { get; set; }
+        public string nextGameStatusText { get; set; }
+        public int nextGameBroadcasterId { get; set; }
+        public string nextGameBroadcasterDisplay { get; set; }
+    }
+
+
     public class Bracket
     {
         public string leagueId { get; set; }
         public string seasonYear { get; set; }
         public string bracketType { get; set; }
         public List<PlayoffBracketSeries> playoffBracketSeries { get; set; }
+        public List<PlayInBracketSeries> playInBracketSeries { get; set; }
     }
 
     public class PlayoffBracketResponse
