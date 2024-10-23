3732003ab65b8b8244f5f1475505ab0d7e230109
jakesjordan00 <119546212+jakesjordan00@users.noreply.github.com>
2024-10-23 16:54:12 -0400

    test

--- Site.Master.cs
+++ Site.Master.cs
@@ -13,135 +13,139 @@ namespace NBAdb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ScoresRow.Controls.Clear();
-            var client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
-            string endpoint = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json";
-            try
+            if (!this.IsPostBack)
             {
-                WebRequest BoxScoreReq = WebRequest.Create(endpoint);
-                WebResponse BoxScoreResp = BoxScoreReq.GetResponse();
-                string json = client.DownloadString(endpoint);
-                Root JSON = JsonConvert.DeserializeObject<Root>(json);
-                for (int i = 0; i < JSON.Scoreboard.Games.Count; i++)
+                ScoresRow.Controls.Clear();
+                var client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
+                string endpoint = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json";
+                try
                 {
-                    // Create a container div with class 'col-md-4'
-                    Panel colDiv = new Panel();
-                    colDiv.CssClass = "col-md-3";
-                    colDiv.Attributes.Add("style", "width:150px");
+                    WebRequest BoxScoreReq = WebRequest.Create(endpoint);
+                    WebResponse BoxScoreResp = BoxScoreReq.GetResponse();
+                    string json = client.DownloadString(endpoint);
+                    Root JSON = JsonConvert.DeserializeObject<Root>(json);
+                    for (int i = 0; i < JSON.Scoreboard.Games.Count; i++)
+                    {
+                        // Create a container div with class 'col-md-4'
+                        Panel colDiv = new Panel();
+                        colDiv.CssClass = "col-md-3";
+                        colDiv.Attributes.Add("style", "width:150px");
 
-                    // First row
-                    Panel rowDiv1 = new Panel();
-                    rowDiv1.Attributes.Add("class", "row");
-                    rowDiv1.Attributes.Add("style", "width:150px");
+                        // First row
+                        Panel rowDiv1 = new Panel();
+                        rowDiv1.Attributes.Add("class", "row");
+                        rowDiv1.Attributes.Add("style", "width:150px");
 
-                    Panel colDiv1_1 = new Panel();
-                    colDiv1_1.Attributes.Add("style", "width:75px; text-align:left; font-size: small");
+                        Panel colDiv1_1 = new Panel();
+                        colDiv1_1.Attributes.Add("style", "width:75px; text-align:left; font-size: small");
 
-                    Label label1 = new Label();
-                    label1.ID = "GameClock" + i; 
-                    label1.Text = JSON.Scoreboard.Games[i].GameClock; 
+                        Label label1 = new Label();
+                        label1.ID = "GameClock" + i;
+                        label1.Text = JSON.Scoreboard.Games[i].GameClock;
 
-                    colDiv1_1.Controls.Add(label1);
-
-
-
-                    Panel colDiv1_2 = new Panel();
-                    colDiv1_2.Attributes.Add("style", "width:75px; text-align:right; font-size: small");
-
-                    Label label2 = new Label();
-                    label2.ID = "GameStart" + i;
-                    label2.Text = JSON.Scoreboard.Games[i].GameEt.ToShortTimeString();
-
-                    colDiv1_2.Controls.Add(label2);
-
-                    rowDiv1.Controls.Add(colDiv1_1);
-                    rowDiv1.Controls.Add(colDiv1_2);
+                        colDiv1_1.Controls.Add(label1);
 
 
 
-                    // Second row
-                    Panel rowDiv2 = new Panel();
-                    rowDiv2.Attributes.Add("class", "row");
-                    rowDiv2.Attributes.Add("style", "width:150px");
+                        Panel colDiv1_2 = new Panel();
+                        colDiv1_2.Attributes.Add("style", "width:75px; text-align:right; font-size: small");
 
-                    Panel colDiv2_1 = new Panel();
-                    colDiv2_1.Attributes.Add("style", "width:75px; text-align:left");
+                        Label label2 = new Label();
+                        label2.ID = "GameStart" + i;
+                        label2.Text = JSON.Scoreboard.Games[i].GameEt.ToShortTimeString();
 
-                    Image teamIcon = new Image();
-                    teamIcon.ImageUrl = "https://cdn.nba.com/logos/nba/streams/L/nss-" + JSON.Scoreboard.Games[i].HomeTeam.TeamTricode.ToLower() + "-studio.png"; // Set dynamic image URL
-                    teamIcon.Width = Unit.Pixel(20); // Adjust size to your preference
-                    teamIcon.Height = Unit.Pixel(20);
+                        colDiv1_2.Controls.Add(label2);
 
-                    Label label3 = new Label();
-                    label3.ID = "sbTeam" + i;
-                    label3.Text = JSON.Scoreboard.Games[i].HomeTeam.TeamTricode;
-
-                    colDiv2_1.Controls.Add(teamIcon);
-                    colDiv2_1.Controls.Add(label3);
-
-                    Panel colDiv2_2 = new Panel();
-                    colDiv2_2.Attributes.Add("style", "width:75px; text-align:right");
-
-                    Label label4 = new Label();
-                    label4.ID = "sbTeamPts" + i;
-                    label4.Text = JSON.Scoreboard.Games[i].HomeTeam.Score.ToString();
-
-                    colDiv2_2.Controls.Add(label4);
-
-                    rowDiv2.Controls.Add(colDiv2_1);
-                    rowDiv2.Controls.Add(colDiv2_2);
+                        rowDiv1.Controls.Add(colDiv1_1);
+                        rowDiv1.Controls.Add(colDiv1_2);
 
 
-                    // Second row
-                    Panel rowDiv3 = new Panel();
-                    rowDiv3.Attributes.Add("class", "row");
-                    rowDiv3.Attributes.Add("style", "width:150px");
 
-                    Panel colDiv3_1 = new Panel();
-                    colDiv3_1.Attributes.Add("style", "width:75px; text-align:left");
+                        // Second row
+                        Panel rowDiv2 = new Panel();
+                        rowDiv2.Attributes.Add("class", "row");
+                        rowDiv2.Attributes.Add("style", "width:150px");
+
+                        Panel colDiv2_1 = new Panel();
+                        colDiv2_1.Attributes.Add("style", "width:75px; text-align:left");
+
+                        Image teamIcon = new Image();
+                        teamIcon.ImageUrl = "https://cdn.nba.com/logos/nba/streams/L/nss-" + JSON.Scoreboard.Games[i].HomeTeam.TeamTricode.ToLower() + "-studio.png"; // Set dynamic image URL
+                        teamIcon.Width = Unit.Pixel(20); // Adjust size to your preference
+                        teamIcon.Height = Unit.Pixel(20);
+
+                        Label label3 = new Label();
+                        label3.ID = "sbTeam" + i;
+                        label3.Text = JSON.Scoreboard.Games[i].HomeTeam.TeamTricode;
+
+                        colDiv2_1.Controls.Add(teamIcon);
+                        colDiv2_1.Controls.Add(label3);
+
+                        Panel colDiv2_2 = new Panel();
+                        colDiv2_2.Attributes.Add("style", "width:75px; text-align:right");
+
+                        Label label4 = new Label();
+                        label4.ID = "sbTeamPts" + i;
+                        label4.Text = JSON.Scoreboard.Games[i].HomeTeam.Score.ToString();
+
+                        colDiv2_2.Controls.Add(label4);
+
+                        rowDiv2.Controls.Add(colDiv2_1);
+                        rowDiv2.Controls.Add(colDiv2_2);
 
 
-                    Image teamIcon2 = new Image();
-                    teamIcon2.ImageUrl = "https://cdn.nba.com/logos/nba/streams/L/nss-" + JSON.Scoreboard.Games[i].AwayTeam.TeamTricode.ToLower() + "-studio.png"; // Set dynamic image URL
-                    teamIcon2.Width = Unit.Pixel(20); // Adjust size to your preference
-                    teamIcon2.Height = Unit.Pixel(20);
-                    Label label5 = new Label();
-                    label5.ID = "sbTeam" + i;
-                    label5.Text = JSON.Scoreboard.Games[i].AwayTeam.TeamTricode;
+                        // Second row
+                        Panel rowDiv3 = new Panel();
+                        rowDiv3.Attributes.Add("class", "row");
+                        rowDiv3.Attributes.Add("style", "width:150px");
 
-                    colDiv3_1.Controls.Add(teamIcon2);
-                    colDiv3_1.Controls.Add(label5);
-
-                    Panel colDiv3_2 = new Panel();
-                    colDiv3_2.Attributes.Add("style", "width:75px; text-align:right");
-
-                    Label label6 = new Label();
-                    label6.ID = "sbTeamPts" + i;
-                    label6.Text = JSON.Scoreboard.Games[i].AwayTeam.Score.ToString();
-
-                    colDiv3_2.Controls.Add(label6);
-
-                    rowDiv3.Controls.Add(colDiv3_1);
-                    rowDiv3.Controls.Add(colDiv3_2);
+                        Panel colDiv3_1 = new Panel();
+                        colDiv3_1.Attributes.Add("style", "width:75px; text-align:left");
 
 
-                    // Add both rows to the main column div
-                    colDiv.Controls.Add(rowDiv1);
-                    colDiv.Controls.Add(rowDiv2);
-                    colDiv.Controls.Add(rowDiv3);
+                        Image teamIcon2 = new Image();
+                        teamIcon2.ImageUrl = "https://cdn.nba.com/logos/nba/streams/L/nss-" + JSON.Scoreboard.Games[i].AwayTeam.TeamTricode.ToLower() + "-studio.png"; // Set dynamic image URL
+                        teamIcon2.Width = Unit.Pixel(20); // Adjust size to your preference
+                        teamIcon2.Height = Unit.Pixel(20);
+                        Label label5 = new Label();
+                        label5.ID = "sbTeam" + i;
+                        label5.Text = JSON.Scoreboard.Games[i].AwayTeam.TeamTricode;
 
-                    // Finally, add the column div to the ScoresContainer
-                    ScoresRow.Controls.Add(colDiv); // ScoresContainer is the container div with runat="server"
-                    ScoresRow.Attributes.Add("style", "overflow-x: auto; overflow-y:auto; white-space: nowrap;" +
-                        " max-width:1320px; width:1320px;" +
-                        " max-height:87.5px; width:100%; display:flex;");
+                        colDiv3_1.Controls.Add(teamIcon2);
+                        colDiv3_1.Controls.Add(label5);
+
+                        Panel colDiv3_2 = new Panel();
+                        colDiv3_2.Attributes.Add("style", "width:75px; text-align:right");
+
+                        Label label6 = new Label();
+                        label6.ID = "sbTeamPts" + i;
+                        label6.Text = JSON.Scoreboard.Games[i].AwayTeam.Score.ToString();
+
+                        colDiv3_2.Controls.Add(label6);
+
+                        rowDiv3.Controls.Add(colDiv3_1);
+                        rowDiv3.Controls.Add(colDiv3_2);
+
+
+                        // Add both rows to the main column div
+                        colDiv.Controls.Add(rowDiv1);
+                        colDiv.Controls.Add(rowDiv2);
+                        colDiv.Controls.Add(rowDiv3);
+
+                        // Finally, add the column div to the ScoresContainer
+                        ScoresRow.Controls.Add(colDiv); // ScoresContainer is the container div with runat="server"
+                        ScoresRow.Attributes.Add("style", "overflow-x: auto; overflow-y:auto; white-space: nowrap;" +
+                            " max-width:1320px; width:1320px;" +
+                            " max-height:87.5px; width:100%; display:flex;");
+
+                    }
+                }
+                catch
+                {
 
                 }
             }
-            catch
-            {
-
-            }
+            
         }
 
         public class Meta
