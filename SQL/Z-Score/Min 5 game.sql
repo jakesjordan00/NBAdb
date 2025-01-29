844020d1c9474f0241552fd34be64ab0f9655a4f
JJ <jakesjordan00@gmail.com>
2025-01-29 16:14:58 -0500

    Commit of recent changes and game notes and such

---
+++ SQL/Z-Score/Min 5 game.sql
@@ -0,0 +1,120 @@
+select s.game_id, e.name Team, m.name Matchup, s.dateTime time, concat('=HYPERLINK(""https://www.nba.com/stats/player/', t.player_id,'/boxscores-traditional"", ""', t.name, '"")') Player, t.Minutes, d5.MinDeviation,
+case when cast((10 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((10 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((10 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [10+],
+case when cast((11 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((11 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((11 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [11+],
+case when cast((12 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((12 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((12 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [12+],
+case when cast((13 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((13 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((13 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [13+],
+case when cast((14 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((14 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((14 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [14+],
+case when cast((15 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((15 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((15 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [15+],
+case when cast((16 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((16 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((16 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [16+],
+case when cast((17 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((17 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((17 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [17+],
+case when cast((18 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((18 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((18 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [18+],
+case when cast((19 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((19 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((19 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [19+],
+case when cast((20 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((20 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((20 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [20+],
+case when cast((21 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((21 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((21 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [21+],
+case when cast((22 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((22 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((22 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [22+],
+case when cast((23 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((23 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((23 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [23+],
+case when cast((24 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((24 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((24 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [24+],
+case when cast((25 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((25 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((25 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [25+],
+case when cast((26 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((26 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((26 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [26+],
+case when cast((27 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((27 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((27 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [27+],
+case when cast((28 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((28 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((28 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [28+],
+case when cast((29 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((29 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((29 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [29+],
+case when cast((30 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((30 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((30 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [30+],
+case when cast((31 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((31 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((31 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [31+],
+case when cast((32 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((32 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((32 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [32+],
+case when cast((33 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((33 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((33 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [33+],
+case when cast((34 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((34 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((34 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [34+],
+case when cast((35 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((35 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((35 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [35+],
+case when cast((40 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((40 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((40 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [40+],
+case when cast((45 - t.Minutes)/d5.MinDeviation as decimal(18,2)) <= -4 then 1 
+when cast((45 - t.Minutes)/d5.MinDeviation as decimal(18,2)) >= 4 then 0
+else (select 1 - CumulativeProbability from StandardNormalDistribution where ZScore = cast((45 - t.Minutes)/d5.MinDeviation as decimal(18,2)))
+end [45+]
+from PlayerTrend5 t inner join
+		PlayerTrend5StdDeviation d5 on t.player_id = d5.player_id inner join
+		team e on t.team_id = e.team_id and e.season_id = 2024 inner join
+		GameSchedule s on (t.team_id = s.home_id or t.team_id = s.away_id) and s.date = cast(getdate() as date) inner join
+		team m on (s.home_id = m.team_id or s.away_id = m.team_id) and e.team_id != m.team_id  and e.season_id = m.season_id
+where t.Minutes >= 8 and d5.MinDeviation != 0
+order by datetime, s.game_id, team, [10+] desc
